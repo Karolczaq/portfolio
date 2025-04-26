682f15fdd1d20c26087f239fb06b8c784dd70ca0
Karol <karol.krasonn@gmail.com>
2025-04-26 21:28:36 +0200

    finished a responsive about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,16 +1,65 @@
+import { socialLinks } from "@/utils/socialLinks";
+import { IoIosArrowDown } from "react-icons/io";
+import Image from "next/image";
+
 export default function About() {
+  const handleCopyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text).then(() => {
+      alert(`Copied to clipboard!`);
+    });
+  };
+
   return (
     <div
       id="about"
-      className="w-full h-screen snap-start flex items-center justify-center"
+      className="w-full h-screen snap-start flex flex-col md:flex-row items-center justify-center md:space-y-0 gap-8 relative"
     >
-      <div className="p-6 max-w-2xl bg-zinc-700 rounded-lg">
-        <span className="text-amber-100 text-2xl font-bold break-words">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          convallis ex sit amet dui sodales accumsan. Suspendisse potenti. Donec
-          hendrerit nulla sed feugiat varius. Nullam et egestas risus.
+      <div className="flex flex-col items-center min-w-[192px] min-h-[192px]">
+        <Image
+          src="/photo.jpg"
+          alt="Profile"
+          width={192}
+          height={192}
+          className="object-cover rounded-full"
+        />
+        <span className="mt-2 text-amber-100 text-xl font-bold">
+          Karol Krasoń, <span className="text-zinc-400"> 19</span>
         </span>
       </div>
+
+      <div className="p-6 max-w-xl bg-zinc-700 rounded-lg text-center mx-4 md:mx-0">
+        <span className="text-amber-100 text-lg font-bold break-words">
+          Hello! I’m an aspiring{" "}
+          <span className="text-amber-400">full-stack</span> developer with a
+          passion for creating efficient, scalable, and user-friendly
+          applications. My journey in software development has been driven by a
+          deep curiosity for how technology can solve real-world problems and
+          make everyday life easier.
+        </span>
+      </div>
+
+      <div className="flex flex-row md:flex-col space-x-4 md:space-x-0 md:space-y-4 font-bold">
+        {socialLinks.map((link) => (
+          <a
+            key={link.name}
+            href={link.href}
+            onClick={(e) => {
+              e.preventDefault();
+              if (link.copyText) {
+                handleCopyToClipboard(link.copyText);
+              }
+            }}
+            className={`${link.className} flex items-center justify-center space-x-0 md:justify-start md:space-x-4`}
+          >
+            <link.icon className="text-amber-100 -m-1 md:mr-2" size="35" />
+            <span className="hidden md:inline">{link.name}</span>{" "}
+            <link.sideIcon className="hidden md:inline" />
+          </a>
+        ))}
+      </div>
+      <a href="#projects" className="p-4 absolute bottom-15 animate-bounce">
+        <IoIosArrowDown className="text-5xl text-amber-100" />
+      </a>
     </div>
   );
 }
---
+++ src/utils/socialLinks.ts
@@ -0,0 +1,32 @@
+import { MdMailOutline } from "react-icons/md";
+import { IoLogoGithub } from "react-icons/io";
+import { FaDiscord } from "react-icons/fa";
+import { FiExternalLink } from "react-icons/fi";
+import { FaCopy } from "react-icons/fa";
+
+export const socialLinks = [
+  {
+    name: "GitHub",
+    icon: IoLogoGithub,
+    href: "https://github.com/karolczaq",
+    sideIcon: FiExternalLink,
+    className:
+      "p-4 bg-zinc-700 rounded-lg text-center text-amber-100 hover:bg-zinc-600 transition flex items-center space-x-2",
+  },
+  {
+    name: "karolczaq",
+    icon: FaDiscord,
+    copyText: "karolczaq",
+    sideIcon: FaCopy,
+    className:
+      "p-4 bg-zinc-700 rounded-lg text-center text-amber-100 hover:bg-zinc-600 transition flex items-center space-x-2",
+  },
+  {
+    name: "Email",
+    icon: MdMailOutline,
+    copyText: "k.krasonn@gmail.com",
+    sideIcon: FaCopy,
+    className:
+      "p-4 bg-zinc-700 rounded-lg text-center text-amber-100 hover:bg-zinc-600 transition flex items-center space-x-2",
+  },
+];
