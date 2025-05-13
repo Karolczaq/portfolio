394e39d04e146ac17aabeb26c2d848107a0e482c
Karol <karol.krasonn@gmail.com>
2025-05-13 20:52:56 +0200

    minor fix

--- src/components/Projects/ProjectFrame.tsx
+++ src/components/Projects/ProjectFrame.tsx
@@ -81,7 +81,6 @@ const ProjectFrame: React.FC<ProjectFrameProps> = ({ project, onClose }) => {
               <div className="flex items-center justify-center max-w-4xl mx-auto">
                 <div className="w-full p-5 pt-10 text-white">
                   <div className="flex items-center justify-between mb-4">
-                    {/* Project Title and Technologies */}
                     <div>
                       <h1 className="text-4xl font-bold">{project.title}</h1>
                       <div className="flex flex-wrap gap-2 mt-2">
@@ -100,8 +99,6 @@ const ProjectFrame: React.FC<ProjectFrameProps> = ({ project, onClose }) => {
                         ))}
                       </div>
                     </div>
-
-                    {/* Links to GitHub and Live URL */}
                     <div className="flex items-center gap-4 font-bold">
                       {project.githubUrl && (
                         <a
@@ -131,43 +128,41 @@ const ProjectFrame: React.FC<ProjectFrameProps> = ({ project, onClose }) => {
                       remarkPlugins={[remarkGfm]}
                       rehypePlugins={[rehypeRaw]}
                       components={{
-                        h1: ({ node, ...props }) => (
+                        h1: ({ ...props }) => (
                           <h1
                             className="py-10 text-3xl font-bold lg:text-4xl"
                             {...props}
                           />
                         ),
-                        h2: ({ node, ...props }) => (
+                        h2: ({ ...props }) => (
                           <h2
                             className="py-2 font-medium text-md lg:text-xl xl:py-5"
                             {...props}
                           />
                         ),
-                        table: ({ node, ...props }) => (
+                        table: ({ ...props }) => (
                           <table
                             className="w-full px-2 overflow-hidden text-center border-collapse rounded-lg xl:p-2"
                             {...props}
                           />
                         ),
-                        thead: ({ node, ...props }) => (
+                        thead: ({ ...props }) => (
                           <thead
                             className="border-b bg-neutral-700 border-neutral-500 text-neutral-50"
                             {...props}
                           />
                         ),
-                        tbody: ({ node, ...props }) => (
+                        tbody: ({ ...props }) => (
                           <tbody className="bg-neutral-800" {...props} />
                         ),
-                        tr: ({ node, ...props }) => (
+                        tr: ({ ...props }) => (
                           <tr className="p-1 text-neutral-200" {...props} />
                         ),
-                        td: ({ node, ...props }) => (
-                          <td className="p-1" {...props} />
-                        ),
-                        th: ({ node, ...props }) => (
+                        td: ({ ...props }) => <td className="p-1" {...props} />,
+                        th: ({ ...props }) => (
                           <th className="p-1 py-2" {...props} />
                         ),
-                        pre: ({ node, ...props }) => (
+                        pre: ({ ...props }) => (
                           <pre
                             className="p-2 my-2 text-sm whitespace-pre-wrap rounded-lg bg-neutral-800 text-neutral-200"
                             {...props}
